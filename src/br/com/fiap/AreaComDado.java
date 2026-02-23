970821fe32a787f6fc6f1fb4bab815e3a1ec8f7b
guilherme de araujo <guiam2909@gmail.com>
2026-02-23 18:57:04 -0300

    realizando exercicios de area, testando novos metodos para realizar as contas e aestilizacao de respostas

--- src/br/com/fiap/AreaComDado.java
+++ src/br/com/fiap/AreaComDado.java
@@ -1,5 +1,17 @@
 package br.com.fiap;
 
-public class AreaComDado {
+import java.util.Scanner;
 
+public class AreaComDado {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        try {
+            System.out.println("digite o valor do raio do circulo: ");
+            double raio = scan.nextDouble();
+            double area = Math.PI * Math.pow(raio, 2);
+            System.out.printf("A Area do circulo vai ser: %.2f%n", area);
+        } catch (Exception e) {
+            System.out.println("Formato de numero invalido!");
+        }
+    }
 }
--- src/br/com/fiap/AreaComJanela.java
+++ src/br/com/fiap/AreaComJanela.java
@@ -1,4 +1,17 @@
 package br.com.fiap;
 
+import javax.swing.*;
+
 public class AreaComJanela {
+    public static void main(String[] args) {
+        try {
+            String auxiliar = JOptionPane.showInputDialog("Digite o valor do raio do circulo: ");
+            double raio = Double.parseDouble(auxiliar);
+            double area = Math.PI * Math.pow(raio, 2);
+            String mensagemMelhorada = String.format("A area do circulo vai ser: %.2f", area);
+            JOptionPane.showMessageDialog(null, mensagemMelhorada);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Formato numerico invalido!");
+        }
+    }
 }
