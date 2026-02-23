e23f340b3478acfdea87c11b8c5845b9409cb42c
Guilherme de Araujo Moreira <guiam2909@gmail.com>
2026-02-23 09:28:49 -0300

    Entrada de Dados por janela com auxiliar e JOpP

--- src/br/com/fiap/EntradaDeDado.java
+++ src/br/com/fiap/EntradaDeDado.java
@@ -4,16 +4,18 @@ import java.util.Scanner;
 
 public class EntradaDeDado {
     public static void main(String[] args) {
-        int num1 = 0;
+        int num1 = 0, num2 = 0, resultado = 0;
         // declaração de objeto
-        Scanner scan;
-        // instanciando um objeto
-        scan = new Scanner(System.in);
-        // Scanner scan new Scanner(System.in);
+        Scanner scan; // Scanner scan new Scanner(System.in);
         try {
-            System.out.println("Digite um número inteiro:\n->");
+            // instanciando um objeto
+            scan = new Scanner(System.in);
+            System.out.println("Digite um número inteiro:");
             num1 = scan.nextInt();
-            System.out.println("Você digitou: " + num1);
+            System.out.println("Digite outro numero inteiro:");
+            num2 = scan.nextInt();
+            resultado = num1 + num2;
+            System.out.println("Valor 1: " + num1 + "\nValor 2: " + num2 + "\nSomatório: " + resultado);
         } catch (Exception e) {
             System.out.println("Fomato de número incorreto.");;
         }
